38b6695ba355675cb03ba3f52e951bd10135fd11
Steven Kellner <steven.kellner@web.de>
2023-08-25 13:03:51 +0200

    Added image support for reports

--- src/app/modules/reports/types/ReportMessageParser.ts
+++ src/app/modules/reports/types/ReportMessageParser.ts
@@ -1,57 +1,134 @@
 import { RegexIterable } from 'src/app/types/regex-iterable';
 
+class Elements implements Iterable<string | HTMLElement> {
+
+    public constructor(
+        private elements: (string | HTMLElement)[] = []
+    ) {}
+
+    public static fromContent(content: string): Elements {
+        return new Elements([content]);
+    }
+
+    public [Symbol.iterator](): IterableIterator<string | HTMLElement> {
+        return this.elements[Symbol.iterator]();
+    }
+
+    public get stringElements(): string[] {
+        return this.elements.flatMap(element => typeof element === 'string' ? element : []);
+    }
+
+    public get htmlElements(): HTMLElement[] {
+        return this.elements.flatMap(element => typeof element !== 'string' ? element : []);
+    }
+
+    public push(element: string | HTMLElement) {
+        this.elements.push(element);
+    }
+
+    public copy(elements?: 'string' | 'html' | ['string', 'html'] | ['html', 'string']): Elements {
+        if (elements === undefined)
+            return new Elements();
+        if (elements === 'string')
+            return new Elements(this.stringElements);
+        if (elements === 'html')
+            return new Elements(this.htmlElements);
+        return new Elements(this.elements);
+    }
+}
+
 export class ReportMessageParser {
     public constructor() {}
 
-    public parse(message: string): (string | HTMLElement)[] | null {
-        let elements = this.parseLinks(message);
-        if (elements === null)
-            return null;
-        elements = this.parseNewLines(elements);
-        if (elements === null)
-            return null;
-        elements = this.parseHorizonalLine(elements);
-        if (elements === null)
-            return null;
-        return this.parseFormatting(elements);
-    }
-
-    private parseLinks(message: string): (string | HTMLElement)[] | null {
-        let lastIndex = 0;
-        const elements: (string | HTMLAnchorElement)[] = [];
-        const regexIt = new RegexIterable(/\[(?<content>[\S\s]+?)\]\((?<link>[\S\s]+?)\)/g, message);
-        for (const match of regexIt) {
-            elements.push(message.slice(lastIndex, match.startIndex));
-            if (match.groups === undefined)
-                continue;
-            const link = document.createElement('a');
-            link.href = match.groups['link'];
-            link.target = '_blank';
-            const content = this.parseFormatting([match.groups['content']]);
-            if (content === null)
+    public parse(message: string): Elements | null {
+        const parsers = [
+            this.parseImages, this.parseLinks, this.parseNewLines, this.parseHorizonalLine, this.parseFormatting
+        ];
+        let elements: Elements | null = Elements.fromContent(message);
+        for (const parser of parsers) {
+            if (elements === null)
                 return null;
-            for (const element of content)
-                link.append(element);
-            link.title = link.innerText;
-            elements.push(link);
-            lastIndex = match.endIndex + 1;
+            elements = parser(elements);
         }
-        elements.push(message.slice(lastIndex));
         return elements;
     }
 
-    private parseNewLines(elements: (string | HTMLElement)[]): (string | HTMLElement)[] | null {
-        const result: (string | HTMLElement)[] = [];
-        for (const element of elements) {
-            if (typeof element === 'string') {
-                const regexIt = new RegexIterable(/\n/g, element);
-                let lastIndex = 0;
-                for (const match of regexIt) {
-                    result.push(element.slice(lastIndex, match.startIndex));
-                    result.push(document.createElement('br'));
-                    lastIndex = match.endIndex + 1;
-                }
-                result.push(element.slice(lastIndex));
+    private parseImages(elements: Elements): Elements | null {
+        const result = elements.copy('html');
+        for (const element of elements.stringElements) {
+            let lastIndex = 0;
+            const regexIt = new RegexIterable(/!\[(?<title>[\S\s]+?)\]\((?<link>[\S\s]+?)(?:\s*,\s*(?<width>\d*%)\s*)?\)/g, element);
+            for (const match of regexIt) {
+                result.push(element.slice(lastIndex, match.startIndex));
+                if (match.groups === undefined)
+                    continue;
+                const image = document.createElement('img');
+                image.src = match.groups['link'];
+                image.title = match.groups['title'];
+                image.alt = match.groups['title'];
+                console.log(match.groups);
+                if (match.groups['width'] !== undefined)
+                    image.style.width = match.groups['width'];
+                result.push(image);
+                lastIndex = match.endIndex + 1;
+            }
+            result.push(element.slice(lastIndex));
+
+        }
+        return result;
+    }
+
+    private parseLinks(elements: Elements): Elements | null {
+        const result = elements.copy('html');
+        for (const element of elements.stringElements) {
+            let lastIndex = 0;
+            const regexIt = new RegexIterable(/\[(?<content>[\S\s]+?)\]\((?<link>[\S\s]+?)\)/g, element);
+            for (const match of regexIt) {
+                result.push(element.slice(lastIndex, match.startIndex));
+                if (match.groups === undefined)
+                    continue;
+                const link = document.createElement('a');
+                link.href = match.groups['link'];
+                link.target = '_blank';
+                const content = this.parseFormatting(new Elements([match.groups['content']]));
+                if (content === null)
+                    return null;
+                for (const element of content)
+                    link.append(element);
+                link.title = link.innerText;
+                result.push(link);
+                lastIndex = match.endIndex + 1;
+            }
+            result.push(element.slice(lastIndex));
+
+        }
+        return result;
+    }
+
+    private parseNewLines(elements: Elements): Elements | null {
+        const result = elements.copy('html');
+        for (const element of elements.stringElements) {
+            const regexIt = new RegexIterable(/\n/g, element);
+            let lastIndex = 0;
+            for (const match of regexIt) {
+                result.push(element.slice(lastIndex, match.startIndex));
+                result.push(document.createElement('br'));
+                lastIndex = match.endIndex + 1;
+            }
+            result.push(element.slice(lastIndex));
+        }
+        return result;
+    }
+
+    private parseHorizonalLine(elements: Elements): Elements | null {
+        const result = elements.copy('html');
+        for (const element of elements.stringElements) {
+            const match = /^\s*---\s*$/g.exec(element);
+            if (match !== null) {
+                const line = document.createElement('div');
+                line.classList.add('horizontal-line');
+                line.append(document.createElement('div'));
+                result.push(line);
             } else {
                 result.push(element);
             }
@@ -59,28 +136,8 @@ export class ReportMessageParser {
         return result;
     }
 
-    private parseHorizonalLine(elements: (string | HTMLElement)[]): (string | HTMLElement)[] | null {
-        const result: (string | HTMLElement)[] = [];
-        for (const element of elements) {
-            if (typeof element === 'string') {
-                const match = /^\s*---\s*$/g.exec(element);
-                if (match !== null) {
-                    const line = document.createElement('div');
-                    line.classList.add('horizontal-line');
-                    line.append(document.createElement('div'));
-                    result.push(line);
-                } else {
-                    result.push(element);
-                }
-            } else {
-                result.push(element);
-            }
-        }
-        return result;
-    }
-
-    private parseFormatting(elements: (string | HTMLElement)[]): (string | HTMLElement)[] | null {
-        const result: (string | HTMLElement)[] = [];
+    private parseFormatting(elements: Elements): Elements | null {
+        const result = new Elements();
         let components: ['***' | '**' | '*', (string | HTMLElement)[]][] = [];
         for (const element of elements) {
             if (typeof element !== 'string') {
